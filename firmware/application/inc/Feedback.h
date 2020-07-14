e940e4d8476659a97fc4a3f1386ba842d864846b
Ilya Galkin <hardware.sitara@gmail.com>
2020-07-15 02:08:25 +0400

    new: Добавил фильтрацию и рассчет в Get-функции на входного и выходного токов

--- firmware/application/inc/Feedback.h
+++ firmware/application/inc/Feedback.h
@@ -2,7 +2,7 @@
  * class        Filtering and conversion ADC result                             *
  *                                                                              *
  * file         Feedback.h                                                      *
- * author       Ilya Galkin                                                     *
+ * author       @RedCommissary                                                  *
  * date         23.06.2020                                                      *
  *                                                                              *
  ********************************************************************************/
@@ -61,11 +61,14 @@ class Feedback {
             Adc::Status::stopInputVoltage = true;
             float inputVoltage = FilterWindowMedium::Compute(Adc::inputVoltage, Adc::sizeBuffer, sizeWindow);
             Adc::Status::stopInputVoltage = false;
-            return (voltageDivInput * sampleStepAdc * inputVoltage + staticErrorInputVoltage);
+            return (inputVoltage * voltageDivInput * sampleStepAdc + staticErrorInputVoltage);
         }
 
         static float GetInputCurrent() {
-            return (Adc::inputCurrent[0] * sampleStepAdc / gainCurrentSensor / currentShunt);
+            Adc::Status::stopInputCurrent = true;
+            float inputCurrent = FilterWindowMedium::Compute(Adc::inputCurrent, Adc::sizeBuffer, sizeWindow);
+            Adc::Status::stopInputCurrent = false;
+            return (inputCurrent * sampleStepAdc / gainCurrentSensor / currentShunt);
         }
 
         static float GetOutputVoltage() {
@@ -73,17 +76,20 @@ class Feedback {
                 Adc::Status::stopOutputVoltage = true;
                 float outputVoltage = FilterWindowMedium::Compute(Adc::outputVoltage, Adc::sizeBuffer, sizeWindow);
                 Adc::Status::stopOutputVoltage = false;
-                return (voltageDivOutput24V * sampleStepAdc * outputVoltage);
+                return (outputVoltage * sampleStepAdc * voltageDivOutput24V);
             } else {
                 Adc::Status::stopOutputVoltage = true;
                 float outputVoltage = FilterWindowMedium::Compute(Adc::outputVoltage, Adc::sizeBuffer, sizeWindow);
                 Adc::Status::stopOutputVoltage = false;
-                return (voltageDivOutput12V * sampleStepAdc * outputVoltage);
+                return (outputVoltage * sampleStepAdc * voltageDivOutput12V);
             }
         }
 
         static float GetOutputCurrent() {
-            return (Adc::outputCurrent[0] * sampleStepAdc / gainCurrentSensor / currentShunt);
+            Adc::Status::stopOutputCurrent = true;
+            float outputCurrent = FilterWindowMedium::Compute(Adc::outputCurrent, Adc::sizeBuffer, sizeWindow);
+            Adc::Status::stopOutputCurrent = false;
+            return (outputCurrent * sampleStepAdc / gainCurrentSensor / currentShunt);
         }
 
     private:
